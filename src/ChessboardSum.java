478711d3b66370d29eba52b5fcff10a4360434e3
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-19 20:52:57 +0530

    Submitted CheckBoardSum

--- src/ChessboardSum.java
+++ src/ChessboardSum.java
@@ -1,9 +1,30 @@
+import java.util.*;
 public class ChessboardSum {
-    
-        // TODO: Read N
-        // TODO: Read the matrix
-        // TODO: Iterate through the matrix
-        // TODO: Check if (i + j) is even (Black) or odd (White)
-        // TODO: Accumulate sums and print them
-    
+        private static int[] checkSum(int[][] matrix, int n, int blackSum, int whiteSum) {
+                for(int i=0;i<n;i++){
+                        for(int j=0;j<n;j++){
+                                if((i+j)%2==0){
+                                        blackSum += matrix[i][j];
+                                } else {
+                                        whiteSum += matrix[i][j];
+                                }
+                        }
+                }
+                return new int[]{blackSum, whiteSum};
+
+        }
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int[][] matrix = new int[n][n];
+                for(int i=0;i<n;i++){
+                        for(int j=0;j<n;j++){
+                                matrix[i][j] = sc.nextInt();
+                        }
+                }
+                int[] ans = checkSum(matrix, n, 0, 0);
+                System.out.println(ans[0]);
+                System.out.println(ans[1]);
+        }
+
 }
